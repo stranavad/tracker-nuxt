84a4603333e6c092f7512c9dd343d3e98e3afa1a
Gandalf The Gray <stranavadavid@protonmail.com>
2025-04-21 20:23:31 +0200

    Updated packages

--- app.vue
+++ app.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
-    <div class="w-screen px-2 py-3 border-b border-b-gray-500 mb-2 flex justify-between">
-      <NuxtLink to="/" class="hover:text-slate-500">Shi Mon-ovy deti</NuxtLink>
-      <NuxtLink to="/devices" class="hover:text-slate-500">Devices</NuxtLink>
+  <UApp>
+    <div>
+      <div class="w-screen px-2 py-3 border-b border-b-gray-500 mb-2 flex justify-between">
+        <NuxtLink to="/" class="hover:text-slate-500">Shi Mon-ovy deti</NuxtLink>
+        <NuxtLink to="/devices" class="hover:text-slate-500">Devices</NuxtLink>
+      </div>
+      <div class="px-2">
+        <NuxtPage/>
+      </div>
     </div>
-    <div class="px-2">
-      <NuxtPage/>
-    </div>
-  </div>
+  </UApp>
 </template>
