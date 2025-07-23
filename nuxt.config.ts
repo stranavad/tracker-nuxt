9e731bd20735dc1e604ce81f165e8dbbe6993ffa
Gandalf The Gray <stranavadavid@protonmail.com>
2025-07-23 03:04:56 +0200

    Updated env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,8 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   runtimeConfig: {
     public: {
-      // apiUrl: 'https://tracker-be.gandalfthegray.org'
-      apiUrl: 'http://localhost:8080'
+      apiUrl: 'https://tracker-be.gandalfthegray.org'
+      // apiUrl: 'http://localhost:8080'
     }
   },
   dayjs: {
